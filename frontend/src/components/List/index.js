2d76f1cec7a6798904b08eaf7bc28a889e3a2226
Felipe Valerio de Ramos <felipevalerioderamos@gmail.com>
2021-01-06 16:39:55 -0300

    Criando o fetch da List

--- frontend/src/components/List/index.js
+++ frontend/src/components/List/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, ListGroup } from 'react-bootstrap';
+import api from '../../services/apiRoutes';
 
 function List() {
+
+    const [note, setNote] = useState({
+        title: '',
+        description: '',
+    });
+
+    useEffect(async () => {
+        const result = await api.list();
+        setNote(result.data);
+    }, []);
+
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Card.Title>Suas Notas</Card.Title>
                 <ListGroup variant="flush">
-                    <ListGroup.Item>
+                    
+                        {note.title.map(item => (
+                            <ListGroup.Item key={item.id}>
+                                <p>{item.title}</p>
+                            </ListGroup.Item>
+                        ))}
 
-                    </ListGroup.Item>
                 </ListGroup>
-
             </Card.Body>
         </Card>
     )
